4d5b2b87b6015f7f01893b23abee134d24651089
trieuhue068 <152937634+trieuhue068@users.noreply.github.com>
2023-12-12 01:38:36 +0700

    Update baitap2.sql

--- baitap2.sql
+++ baitap2.sql
@@ -19,6 +19,14 @@ FROM candidates
 where skill IN('Python', 'Tableau', 'PostgreSQL')
 Group by 1
 having count(candidate_id) =3
+  -- Bài 6
+  SELECT
+user_id
+,DATE(Max(post_date))-DATE(Min(Post_date)) as days_between
+FROM posts
+where post_date >='2021-01-01' and post_date < '2022-01-01'
+GROUP BY 1
+having count(user_id) >= 2
 -- bài 7
 SELECT
 card_name
@@ -26,3 +34,40 @@ card_name
 FROM monthly_cards_issued
 Group by 1
 Order by difference
+-- Bài 8
+SELECT
+manufacturer
+,count(drug) AS drug_count
+,ABS (sum(cogs-total_sales)) AS total_loss
+FROM pharmacy_sales
+where total_sales <cogs
+Group by 1
+order by total_loss DESC
+-- đang rối não đoạn sao k phải lấy doanh thu -chi phí nếu lỗ ra số âm mà lại là chi phí trừ doanh thu ?
+-- bài 9
+select
+id,
+movie,
+ description,
+ rating
+ from Cinema
+where id%2=1 and description <> 'boring'
+order by rating desc
+-- Bài 10
+select
+teacher_id,
+count(distinct subject_id) as cnt
+from Teacher
+group by 1
+-- Bài 11
+select
+user_id
+,count(follower_id) as followers_count
+from Followers
+Group by 1
+order by user_id 
+--Bài 12
+select
+class
+from Courses
+having count(class) >=5
